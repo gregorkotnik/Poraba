ad4a12c85225bc015e487e122a3f6c5695bafa88
gregorkotnik <gregorkotnik.feri@gmail.com>
2019-12-06 22:21:30 +0100

    Delete API KEY

--- app/src/main/java/com/example/gigi/poraba/Fragments/LocationFragment.java
+++ app/src/main/java/com/example/gigi/poraba/Fragments/LocationFragment.java
@@ -1,5 +1,30 @@
 package com.example.gigi.poraba.Fragments;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.gigi.poraba.BuildConfig;
+import com.example.gigi.poraba.R;
+import com.example.gigi.poraba.Models.GasStation;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -19,236 +44,228 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.gigi.poraba.Models.GasStation;
-import com.example.gigi.poraba.R;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+public class LocationFragment extends android.support.v4.app.Fragment
+{
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+	Context context = getActivity();
+	String[] values;
+	Button btnGetData;
+	public static ListView listView;
+	public static ProgressBar spinner;
+	TextView tvLat, tvLongi;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+	private final String API_KEY = BuildConfig.API_KEY;
+	private FusedLocationProviderClient fusedLocationClient;
+	private boolean flag = false;
+	static public final int REQUEST_LOCATION = 1;
 
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
 
-public class LocationFragment extends android.support.v4.app.Fragment {
+	}
 
-    Context context=getActivity();
-    String[] values;
-    Button btnGetData;
-    public static ListView listView;
-    public static ProgressBar spinner;
-    TextView tvLat, tvLongi;
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.gasstationslocationfragment, container, false);
+		btnGetData = (Button) view.findViewById(R.id.btnGetData);
+		spinner = (ProgressBar) view.findViewById(R.id.progressBar1);
+		spinner.setVisibility(View.GONE);
+		tvLat = (TextView) view.findViewById(R.id.tvLATITUDE);
+		tvLongi = (TextView) view.findViewById(R.id.tvLONGITUDE);
+		listView = (ListView) view.findViewById(R.id.lvJSON);
+		fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
-    private final String API_KEY = "AIzaSyCCvNVzUotAZ2X-NQwVTQpRPfykSSwd1z0";
-    private FusedLocationProviderClient fusedLocationClient;
-    private boolean flag = false;
-    static public final int REQUEST_LOCATION = 1;
+		btnGetData.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				if (ActivityCompat.checkSelfPermission(getActivity(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+				{
 
+					Toast.makeText(context, "ZAVRNJENO", Toast.LENGTH_LONG).show();
+					requestPermission();
+					return;
+				}
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+				fusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>()
+				{
+					@Override
+					public void onSuccess(Location location)
+					{
 
-    }
+						if (location != null)
+						{
+							FetchData fetchData = new FetchData();
 
+							double Latitude = location.getLatitude();
+							double Longitude = location.getLongitude();
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.gasstationslocationfragment,container,false);
-        btnGetData=(Button)view.findViewById(R.id.btnGetData);
-        spinner =(ProgressBar)view.findViewById(R.id.progressBar1);
-        spinner.setVisibility(View.GONE);
-        tvLat=(TextView)view.findViewById(R.id.tvLATITUDE);
-        tvLongi=(TextView)view.findViewById(R.id.tvLONGITUDE);
-        listView = (ListView) view.findViewById(R.id.lvJSON);
-        fusedLocationClient= LocationServices.getFusedLocationProviderClient(getActivity());
+							fetchData.execute(getActivity(), Latitude, Longitude);
+							tvLongi.setText(String.valueOf(location.getLongitude()));
+							tvLat.setText(String.valueOf(location.getLatitude()));
+						}
+					}
+				});
+			}
+		});
 
-        btnGetData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (ActivityCompat.checkSelfPermission(getActivity(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+			{
+				String text = values[i].toString();
 
+				Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+			}
+		});
 
-                    Toast.makeText(context,"ZAVRNJENO",Toast.LENGTH_LONG).show();
-                    requestPermission();
-                    return;
-                }
+		return view;
+	}
 
-                fusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
+	private void requestPermission()
+	{
+		ActivityCompat.requestPermissions(getActivity(), new String[] { ACCESS_FINE_LOCATION }, 1);
+	}
 
-                        if(location!=null)
-                        {
-                            FetchData fetchData = new FetchData();
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public class FetchData extends AsyncTask<Object, Void, Void>
+	{
 
-                            double Latitude=location.getLatitude();
-                            double Longitude=location.getLongitude();
+		String data;
+		String getSingleData2 = "";
+		String json;
+		List<GasStation> listOfGasStations;
+		ArrayAdapter<String> adapter;
+		ArrayAdapter<GasStation> adapterGasStationLocation;
 
-                            fetchData.execute(getActivity(),Latitude,Longitude);
-                            tvLongi.setText(String.valueOf(location.getLongitude()));
-                            tvLat.setText(String.valueOf(location.getLatitude()));
-                        }
-                    }
-                });
-            }
-        });
+		@Override
+		protected void onPreExecute()
+		{
+			super.onPreExecute();
+			// MainActivity.spinner.setVisibility(View.GONE);
+			spinner.setVisibility(View.VISIBLE);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String text=values[i].toString();
+		}
 
-                Toast.makeText(context,text,Toast.LENGTH_LONG).show();
-            }
-        });
+		@Override
+		protected Void doInBackground(Object... voids)
+		{
+			try
+			{
 
-        return view;
-    }
+				Context ctx = (Context) voids[0];
+				double d1 = (Double) voids[1];
+				double d2 = (Double) voids[2];
 
-    private void requestPermission()
-    {
-        ActivityCompat.requestPermissions(getActivity(),new String[]{ACCESS_FINE_LOCATION},1);
-    }
+				String lat = String.valueOf(d1);
+				String longi = String.valueOf(d2);
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public class FetchData extends AsyncTask<Object,Void,Void> {
+				Location startPoint = new Location("locationA");
+				startPoint.setLatitude(d1);
+				startPoint.setLongitude(d2);
 
-        String data;
-        String getSingleData2="";
-        String json;
-        List<GasStation> listOfGasStations;
-        ArrayAdapter<String> adapter;
-        ArrayAdapter<GasStation> adapterGasStationLocation;
+				URL url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + longi
+						+ "&radius=1500&type=gas_station&rankBy=1500&key=AIzaSyCCvNVzUotAZ2X-NQwVTQpRPfykSSwd1z0");
+				HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+				InputStream inputStream = httpURLConnection.getInputStream();
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));// ,"utf-8"),8);
+				StringBuilder sb = new StringBuilder();
+				String line = null;
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            //MainActivity.spinner.setVisibility(View.GONE);
-            spinner.setVisibility(View.VISIBLE);
+				while ((line = bufferedReader.readLine()) != null)
+				{
+					sb.append(line);
+				}
+				inputStream.close();
+				json = sb.toString();
 
-        }
+				JSONObject jsonObject = new JSONObject(json);
+				JSONArray JA1 = (JSONArray) jsonObject.get("results");
+				// JSONObject JO1= (JSONObject) JA1.get(0);
+				// JSONObject JO2= (JSONObject) JO1.get("geometry");
+				// doloci velikost arrayem
+				values = new String[JA1.length()];
+				listOfGasStations = new ArrayList<>();
 
-        @Override
-        protected Void doInBackground(Object... voids) {
-            try {
+				// parseData=JO2.toString();
+				for (int i = 0; i < JA1.length(); i++)
+				{
+					JSONObject JO = (JSONObject) JA1.get(i);
+					double d3, d4;
+					JSONObject JO2 = (JSONObject) JO.get("geometry");
+					JSONObject JO3 = (JSONObject) JO2.get("location");
+					Location endPoint = new Location("locationA");
+					d3 = Double.valueOf(JO3.getDouble("lat"));
+					d4 = Double.valueOf(JO3.getDouble("lng"));
+					endPoint.setLatitude(d3);
+					endPoint.setLongitude(d4);
 
-                Context ctx=(Context) voids[0];
-                double  d1=(Double)voids[1];
-                double  d2=(Double)voids[2];
+					double distance = (double) startPoint.distanceTo(endPoint);
+					getSingleData2 = JO.get("name") + "\n";
 
-                String lat=String.valueOf(d1);
-                String longi=String.valueOf(d2);
+					listOfGasStations.add(new GasStation(getSingleData2, distance));
 
-                Location startPoint=new Location("locationA");
-                startPoint.setLatitude(d1);
-                startPoint.setLongitude(d2);
+				}
 
-                URL url=new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+longi+"&radius=1500&type=gas_station&rankBy=1500&key=AIzaSyCCvNVzUotAZ2X-NQwVTQpRPfykSSwd1z0");
-                HttpURLConnection httpURLConnection= (HttpURLConnection) url.openConnection();
-                InputStream inputStream=httpURLConnection.getInputStream();
-                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));//,"utf-8"),8);
-                StringBuilder sb=new StringBuilder();
-                String line=null;
+				for (int i = 0; i < listOfGasStations.size(); i++)
+				{
+					// Log.d("test: "+i,values[i]);
+					Log.d("GasLIST", String.valueOf(listOfGasStations.get(i).getDistance()));
+				}
+				for (int i = 0; i < listOfGasStations.size(); i++)
+				{
+					Log.d("Unsorted", String.valueOf(listOfGasStations.get(i).getDistance()));
+				}
+				Collections.sort(listOfGasStations, new Comparator<GasStation>()
+				{
+					@Override
+					public int compare(GasStation gasStation, GasStation t1)
+					{
+						return Double.compare(gasStation.getDistance(), t1.getDistance());
+					}
+				});
+				for (int i = 0; i < listOfGasStations.size(); i++)
+				{
+					double distance = listOfGasStations.get(i).getDistance();
+					int distanceFinal = (int) distance;
+					Log.d("Sorted" + i, String.valueOf(listOfGasStations.get(i).getDistance()));
+					values[i] = String.valueOf(listOfGasStations.get(i).getAdress() + String.valueOf(distanceFinal) + " m");
+				}
+				adapter = new ArrayAdapter<String>(ctx, android.R.layout.simple_list_item_1, values);
+				adapterGasStationLocation = new ArrayAdapter<GasStation>(ctx, android.R.layout.simple_list_item_1, listOfGasStations);
+			}
+			catch (MalformedURLException e)
+			{
+				e.printStackTrace();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			catch (JSONException e)
+			{
+				e.printStackTrace();
+			}
+			return null;
+		}
 
-                while ((line=bufferedReader.readLine())!=null)
-                {
-                    sb.append(line);
-                }
-                inputStream.close();
-                json=sb.toString();
+		@Override
+		protected void onPostExecute(Void aVoid)
+		{
+			super.onPostExecute(aVoid);
 
-                JSONObject jsonObject=new JSONObject(json);
-                JSONArray JA1= (JSONArray) jsonObject.get("results");
-                //JSONObject JO1= (JSONObject) JA1.get(0);
-                //JSONObject JO2= (JSONObject) JO1.get("geometry");
-                //doloci velikost arrayem
-                values=new String[JA1.length()];
-                listOfGasStations=new ArrayList<>();
+			// MainActivity.tvData.setText(this.parseData);
+			listView.setAdapter(adapter);
+			spinner.setVisibility(View.GONE);
+			// mainActivity.spinner.setVisibility(View.GONE);
 
-                //parseData=JO2.toString();
-                for(int i=0;i<JA1.length();i++)
-                {
-                    JSONObject JO=(JSONObject)JA1.get(i);
-                    double d3,d4;
-                    JSONObject JO2=(JSONObject)JO.get("geometry");
-                    JSONObject JO3=(JSONObject)JO2.get("location");
-                    Location endPoint=new Location("locationA");
-                    d3=Double.valueOf(JO3.getDouble("lat"));
-                    d4=Double.valueOf(JO3.getDouble("lng"));
-                    endPoint.setLatitude(d3);
-                    endPoint.setLongitude(d4);
+		}
 
-                    double distance= (double) startPoint.distanceTo(endPoint);
-                    getSingleData2=JO.get("name")+"\n";
-
-                    listOfGasStations.add(new GasStation(getSingleData2,distance));
-
-                }
-
-
-
-
-                for (int i=0;i<listOfGasStations.size();i++)
-                {
-                    //Log.d("test: "+i,values[i]);
-                    Log.d("GasLIST",String.valueOf(listOfGasStations.get(i).getDistance()));
-                }
-                for(int i=0;i<listOfGasStations.size();i++)
-                {
-                    Log.d("Unsorted",String.valueOf(listOfGasStations.get(i).getDistance()));
-                }
-                Collections.sort(listOfGasStations, new Comparator<GasStation>() {
-                    @Override
-                    public int compare(GasStation gasStation, GasStation t1) {
-                        return Double.compare(gasStation.getDistance(),t1.getDistance());
-                    }
-                });
-                for(int i=0;i<listOfGasStations.size();i++)
-                {
-                    double distance=listOfGasStations.get(i).getDistance();
-                    int distanceFinal= (int) distance;
-                    Log.d("Sorted"+i,String.valueOf(listOfGasStations.get(i).getDistance()));
-                    values[i]=String.valueOf(listOfGasStations.get(i).getAdress()
-                            +String.valueOf(distanceFinal)+" m");
-                }
-                adapter=new ArrayAdapter<String>(ctx,android.R.layout.simple_list_item_1,values);
-                adapterGasStationLocation=new ArrayAdapter<GasStation>(ctx,android.R.layout.simple_list_item_1,listOfGasStations);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
-            //MainActivity.tvData.setText(this.parseData);
-            listView.setAdapter(adapter);
-            spinner.setVisibility(View.GONE);
-            //mainActivity.spinner.setVisibility(View.GONE);
-
-        }
-
-
-    }
+	}
 }
